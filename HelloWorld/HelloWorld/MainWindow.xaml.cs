814634b19cc5107d62f75df4fc3858cad60b8062
Лев <l_saltovskiy@mail.ru>
2020-08-25 19:02:24 +0300

    кликер

---
+++ HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+    /// </summary>
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    public partial class MainWindow : Window
+    {
+        int clicks;
+        public MainWindow()
+        {
+            InitializeComponent();
+            clicks = 2+3;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // counter.Text = "зачем ты нажал !!!!!!!!!!!!!!!!!!"; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
+            clicks = clicks + 1;
+            counter.FontSize = clicks;
+            counter.Text = clicks.ToString(); ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
+
+        }
+    }
+}
+
+
+
+
+
