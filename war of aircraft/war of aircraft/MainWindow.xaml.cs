583df23ac5728bb3f4660a19e315f57b14b52687
gggernestggg <70217223+gggernestggg@users.noreply.github.com>
2022-03-15 18:14:27 +0300

    попрыгун

--- war of aircraft/war of aircraft/MainWindow.xaml.cs
+++ war of aircraft/war of aircraft/MainWindow.xaml.cs
@@ -27,10 +27,10 @@ namespace war_of_aircraft
         IGameScreenLayout Lay; // Разметка окна (определяет, как именно разместятся карта и область меню)
         UniversalMap_Wpf Map; // Карта
         CellMapInfo MapInfo; // Параметры карты
-        InventoryPanel hppanel1; // Панель предметов
-        InventoryPanel hppanel2; // Панель предметов
-        InventoryPanel Items1; // Панель предметов
-        InventoryPanel Items2; // Панель предметов
+        InventoryPanel hppanel; // Панель предметов
+        //InventoryPanel hppanel2; // Панель предметов
+        //InventoryPanel Items1; // Панель предметов
+        //InventoryPanel Items2; // Панель предметов
         GameObject player;
         TimerController timer = new TimerController();
         List<GameObject> bullets = new List<GameObject>();
@@ -38,6 +38,15 @@ namespace war_of_aircraft
         Random r = new Random();
         List<GameObject> misha = new List<GameObject>();
         MediaPlayer Audio = new MediaPlayer();
+        List<GameObject> enemies = new List<GameObject>();
+        List<GameObject> enemyBull = new List<GameObject>();//список снарядов враговp
+        List<GameObject> heals = new List<GameObject>();//cпписок аптек
+        List<GameObject> box = new List<GameObject>();//cпписок аптек
+
+
+        int expEnemy;
+        int expMine;
+
         public MainWindow()
         {
             #region Подготовка карты и панели меню
@@ -47,7 +56,7 @@ namespace war_of_aircraft
 
             // Определяем параметры карты: количество клеток по горизонтали и вертикали, размер клетки, 
             // ширина декоративной рамки вокруг карты
-            MapInfo = new CellMapInfo(38, 20, 50, 0);
+            MapInfo = new CellMapInfo(35, 20, 50, 0);
 
             // Создаем карту и размещаем его в окне программы
             Map = MapCreator.GetUniversalMap(this, MapInfo);
@@ -57,30 +66,32 @@ namespace war_of_aircraft
             // Указываем путь к папке с картинками 
             Map.Library.ImagesFolder = new PathInfo { Path = "..\\..\\..\\..\\i", Type = PathType.Relative };
             // Создаем панель инвентаря и размещаем ее в меню
-            hppanel1 = new InventoryPanel(Map.Library, 150);
-            hppanel2 = new InventoryPanel(Map.Library, 150);
-            Items1 = new InventoryPanel(Map.Library, 50);
-            Items2 = new InventoryPanel(Map.Library, 50);
-            Lay.Attach(hppanel1, 1);
-            Lay.Attach(Items1, 1);
-            Lay.Attach(hppanel2, 1);
-            Lay.Attach(Items2, 1);
-            hppanel1.SetBackground(Brushes.Wheat);
+            hppanel = new InventoryPanel(Map.Library, 150);
+            //hppanel2 = new InventoryPanel(Map.Library, 150);
+            //Items1 = new InventoryPanel(Map.Library, 50);
+            //Items2 = new InventoryPanel(Map.Library, 50);
+            Lay.Attach(hppanel, 1);
+            //Lay.Attach(Items1, 1);
+            //Lay.Attach(hppanel2, 1);
+            //Lay.Attach(Items2, 1);
+            hppanel.SetBackground(Brushes.Wheat);
 
             // Создаем текстовую панель и размещаем ее в меню
             Info = new TextArea_Vertical();
             Lay.Attach(Info, 1);
+            Info.AddTextBlock("expEnemy");
+            Info.AddTextBlock("expMine");
 
             // определяем функцию, которая будет вызвана при нажатии на клавишу
             //Map.Keyboard.SetSingleKeyEventHandler(CheckKey);
             #endregion
             player = new GameObject();
-            player.angle = 45;
-            player.x = 50;
             player.y = 50;
             player.speed = 3;
-            player.reload = 1000;
+            player.reload = 100000;
+            player.ammo = 40;
             player.hp = 10;
+            player.InventoryPanel = hppanel;
             addPictures();
             //player.Name = 
             Map.SetMapBackground("map");
@@ -89,11 +100,16 @@ namespace war_of_aircraft
             Start.ShowDialog();
             timer.AddAction(BCE, 10);
             timer.AddAction(mishen, 30000);
+            timer.AddAction(spaunEnemy, 3000);
+            timer.AddAction(sudba, 60000);
+
+            hppanel.AddItem("hp", "hp10");
+            hppanel.AddItem("box", "box", player.ammo.ToString());
         }
 
         void addPictures()
         {
-            Map.Library.AddPicture("map", "map.png");
+            Map.Library.AddPicture("map", "map.jpg");
             Map.Library.AddPicture("player", "player.png");
             Map.Library.AddContainer("player", "player", ContainerType.AutosizedSingleImage);
             player.container = "player";
@@ -103,11 +119,20 @@ namespace war_of_aircraft
             Map.Library.AddPicture("bullet", "bullet.png");
             Map.Library.AddPicture("mishen", "misha.png");
             Map.Library.AddPicture("GO", "Game over.png");
+            Map.Library.AddPicture("enemy", "enemy.png");
+            Map.Library.AddPicture("enemyShoot", "enemyShoot.png");
+            Map.Library.AddPicture("heal", "heal.png");
+            Map.Library.AddPicture("box", "box.png");
             AnimationDefinition animation = new AnimationDefinition();
             for (int i = 0; i < 10; i++)
             {
                 Map.Library.AddPicture("exp" + i, "exp" + i + ".png");
                 animation.AddFrame(100, "exp" + i);
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                Map.Library.AddPicture("hp" + i, "hp" + i + ".png");
             }
             Map.Library.AddAnimation("explosion", animation);
             Map.Library.AddPicture("0", "exp10.png");
@@ -116,6 +141,21 @@ namespace war_of_aircraft
         }
 
         void BCE()
+        {
+            chekKey();
+            shoot();
+            MoveShots();
+            CheckPlayerHit();
+            ReloadMine();
+            enemyShoot();
+            reloadEnemy();
+            reloadPlayer();
+            GO();
+            getHeal();
+            getAmmo();
+        }
+
+        void chekKey()
         {
             if (Map.Keyboard.IsKeyPressed(Key.A))
             {
@@ -146,7 +186,10 @@ namespace war_of_aircraft
                 player.y -= player.dy / 3;
                 Map.ContainerSetCoordinate("player", player.x, player.y);
             }
+        }
 
+        void GO()
+        {
             if (player.hp <= 0)
             {
                 Map.Library.AddContainer("GO", "GO");
@@ -155,19 +198,31 @@ namespace war_of_aircraft
                 timer.RemoveAction(BCE, 10);
                 timer.RemoveAction(mishen, 30000);
             }
+        }
 
-            shoot();
-            MoveShots();
-            CheckHit();
-            ReloadMine();
-
+    void reloadPlayer()
+        {
             player.reload += 50;
+        }
+
+        void reloadEnemy()
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].reload += 100;
+            }
         }
 
         void shoot()
         {
-            if (Map.Keyboard.IsKeyPressed(Key.Space) && player.reload >= 1000)
+            if (Map.Keyboard.IsKeyPressed(Key.Space) && player.reload >= 1000 && player.ammo > 0)
             {
+                string FileName = "C:\\git\\labirint\\war of aircraft\\3ByKN\\shot.mp3";
+                Audio.Open(new Uri(FileName));
+                Audio.Play();
+                player.ammo--;
+                hppanel.SetText("box", player.ammo.ToString());
+
                 GameObject bullet = new GameObject();
                 bullet.x = player.x;
                 bullet.y = player.y;
@@ -204,6 +259,22 @@ namespace war_of_aircraft
                 {
                     Map.ContainerErase(bullets[i].container);
                     bullets.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < enemyBull.Count; i++)
+            {
+                if (enemyBull[i].x >= 0 && enemyBull[i].y >= 0 && enemyBull[i].x <= 1900 && enemyBull[i].y <= 1000)
+                {
+                    enemyBull[i].x = enemyBull[i].x + enemyBull[i].dx;
+                    enemyBull[i].y = enemyBull[i].y + enemyBull[i].dy;
+                    Map.ContainerSetCoordinate(enemyBull[i].container, enemyBull[i].x, enemyBull[i].y);
+                }
+
+                else
+                {
+                    Map.ContainerErase(enemyBull[i].container);
+                    enemyBull.RemoveAt(i);
                 }
             }
         }
@@ -243,7 +314,7 @@ namespace war_of_aircraft
                         Audio.Open(new Uri(FileName));
                         Audio.Play();
                         misha.RemoveAt(i);
-                        player.hp = player.hp - 5.0;
+                        player.getDamage("", 5);
                     }
                 }
 
@@ -264,7 +335,7 @@ namespace war_of_aircraft
              */
         }
 
-        void CheckHit()
+        void CheckPlayerHit()
         {
             for(int i = 0; i < bullets.Count; i++)
             {
@@ -272,19 +343,175 @@ namespace war_of_aircraft
                 {
                     if (Map.CollisionContainers(bullets[i].container, misha[j].container))
                     {
-                        Map.ContainerErase(misha[j].container);
+                        Map.ContainerSetFrame(misha[j].container, "box");
+                        Map.ContainerSetMaxSide(misha[j].container, 70);
+
+                        box.Add(misha[j]);
+                 
                         misha.RemoveAt(j);
                         Map.ContainerErase(bullets[i].container);
                         bullets.RemoveAt(i);
+                        expMine = expMine + 1;
+                        Info.SetText("expMine", "взрывов " + expMine);
+
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < enemyBull.Count; i++)
+            {
+                if (Map.CollisionContainers(enemyBull[i].container, player.container))
+                {
+                    Map.ContainerErase(enemyBull[i].container);
+                    enemyBull.RemoveAt(i);
+                    player.hp = player.hp - 1;
+
+                    hppanel.SetImage("hp", "hp" + player.hp);
+
+                    break;
+                }
+            }
+            //ПОПАДАНИЕ ПО ВРАГУ
+
+            for(int i = 0; i < enemies.Count; i++)
+            {
+                for (int j = 0; j < bullets.Count; j++)
+                {
+                    if (Map.CollisionContainers(bullets[j].container, enemies[i].container))//!!!
+                    {
+                        enemies[i].hp--;
+
+                        if (enemies[i].hp <= 0)
+                        {
+                            Map.ContainerErase(enemies[i].container);
+                            expEnemy = expEnemy + 1;
+                            Info.SetText("expEnemy", "убийств " + expEnemy);
+                            RepairKit(enemies[i].x, enemies[i].y);
+                            enemies.RemoveAt(i);
+                        }
+
+                        Map.ContainerErase(bullets[j].container);
+                        bullets.RemoveAt(j);
 
                         break;
                     }
                 }
             }
         }
+        
+        void spaunEnemy()
+        {
+            GameObject enemy = new GameObject();
+
+            enemy.unitType = "enemy";
+
+            enemies.Add(enemy);
+
+            int x, y; //в них рандом
+            string container = "enemy" + counter.ToString(); //создание уникального имени контенера
+
+            x = r.Next((int)(player.x) - 300, (int)(player.x) + 300); //получения рандом координат
+            y = r.Next((int)(player.y) - 300, (int)(player.y) + 300); //получения рандом координат
+
+            Map.Library.AddContainer(container, "enemy", ContainerType.AutosizedSingleImage); //
+            Map.ContainerSetMaxSide(container, 150); //
+            Map.ContainerSetCoordinate(container, x, y); //
+            int angle = (int)(GameMath.GetAngleOfVector(player.x - x, player.y - y));
+            Map.ContainerSetAngle(container, angle); //
+            enemy.target = player;
+            counter = counter + 1;
+            enemy.x = x;
+            enemy.y = y;
+            enemy.angle = angle;
+            enemy.hp = 1;
+            enemy.container = container;
+            
+        }
+
+        void enemyShoot()
+        {
+            for (int i = 0; enemies.Count > i; i++)
+            {
+                if (enemies[i].checkShot() && enemies[i].reload >= 10000)
+                {
+                    /*создать обект для выстреса врага*/
+                    GameObject bull = new GameObject();
+                    bull.x = enemies[i].x;
+                    bull.y = enemies[i].y;
+                    bull.angle = enemies[i].angle;
+                    bull.container = "enemySoot" + counter;
+                    bull.speed = player.speed + 10;
+
+                    bull.dx = bull.speed * Math.Cos(GameMath.DegreesToRad(bull.angle));
+                    bull.dy = bull.speed * Math.Sin(GameMath.DegreesToRad(bull.angle));
+                    counter = counter + 1;
+                    Map.Library.AddContainer(bull.container, "enemyShoot", ContainerType.AutosizedSingleImage);
+                    Map.ContainerSetMaxSide(bull.container, 30);
+                    Map.ContainerSetCoordinate(bull.container, enemies[i].x, enemies[i].y);
+                    Map.ContainerSetAngle(bull.container, enemies[i].angle);
+
+                    enemyBull.Add(bull);
+
+                    enemies[i].reload = 0;
+                }
+            }
+        }
+
+        void sudba()
+        {
+            player.getDamage("", 1);
+        }
+
+        void RepairKit(double x, double y)
+        {
+            //создаем аптеку
+            GameObject heal = new GameObject();
+            heal.x = x;
+            heal.y = y;
+            heal.angle = 0;
+            heal.container = "heal" + counter;
+
+            counter = counter + 1;
+            Map.Library.AddContainer(heal.container, "heal", ContainerType.AutosizedSingleImage);
+            Map.ContainerSetMaxSide(heal.container, 200);
+            Map.ContainerSetCoordinate(heal.container, x, y);
+
+            heals.Add(heal);
+        }
+
+        void getHeal()
+        {
+            for (int i = 0; i < heals.Count; i++)
+            {
+                if (Map.CollisionContainers(player.container, heals[i].container))//!!!
+                {
+                    player.getDamage("", -2);
+                    Map.ContainerErase(heals[i].container);
+                    heals.RemoveAt(i);
+                }
+            }
+        }
+
+        void getAmmo()
+        {
+            for (int i = 0; i < box.Count; i++)
+            {
+                if (Map.CollisionContainers(player.container, box[i].container))
+                {
+                    player.ammo = player.ammo + 50;
+                    hppanel.SetText("box", player.ammo.ToString());
+                    Map.ContainerErase(box[i].container);
+                    box.RemoveAt(i);
+                }
+            }
+        }
+
+
     }
     public class GameObject
     {
+        public string unitType;
         public string Name;
         public double x;
         public double y;
@@ -294,10 +521,42 @@ namespace war_of_aircraft
         public double dy;
         public string container;
         public int reload;
-        public double hp;
-        public void getDamage(string damageType, double damage)
+        public int hp;
+        public int ammo;
+        public InventoryPanel InventoryPanel;
+        public GameObject target;
+        /*
+         * проверка наведения врага на игрока
+         * 1 вычесляем угол вектора от врага к играку
+         * 2 
+         */
+        public void getDamage(string damageType, int damage)
         {
-            //hp = hp - 
+            hp = hp - damage;
+            if (hp <= 0)
+            {
+                hp = 0;
+            }
+
+            if (InventoryPanel != null)
+            {
+                if(hp > 10)
+                {
+                    hp = 5;
+                }
+                InventoryPanel.SetImage("hp", "hp" + hp);
+            }
+        }
+
+        public bool checkShot()
+        {
+            bool isAimed = false;
+            double angleToTarget = GameMath.GetAngleOfVector(target.x - x, target.y - y);
+            if (Math.Abs(angleToTarget - angle) < 5)
+            {
+                isAimed = true;
+            }
+            return isAimed;
         }
     }
 }
---
+++ попрыгун/попрыгун/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GameMaps.Layouts;
+using GameMaps;
+
+namespace попрыгун
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IGameScreenLayout Lay; // Разметка окна (определяет, как именно разместятся карта и область меню)
+        UniversalMap_Wpf Map; // Карта
+        CellMapInfo MapInfo; // Параметры карты
+        List<Platform> Platforms = new List<Platform>();
+        TimerController timer = new TimerController();
+        double speed = -1;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            // Определяем вид разметки окна: область карты слева и меню справа
+            Lay = LayoutsFactory.GetLayout(LayoutType.Vertical, this.Content);
+
+            // Определяем параметры карты: количество клеток по горизонтали и вертикали, размер клетки, 
+            // ширина декоративной рамки вокруг карты
+            MapInfo = new CellMapInfo(35, 20, 50, 0);
+
+            // Создаем карту и размещаем его в окне программы
+            Map = MapCreator.GetUniversalMap(this, MapInfo);
+            Lay.Attach(Map, 0);
+            //Map.DrawGrid(); // выводим сетку
+
+            // Указываем путь к папке с картинками 
+            Map.Library.ImagesFolder = new PathInfo { Path = "..\\..\\..\\..\\i", Type = PathType.Relative };
+            // Создаем панель инвентаря и размещаем ее в меню
+
+            addPictures();
+            Map.Keyboard.SetSingleKeyEventHandler(checkKey);
+
+            Map.Library.AddContainer("player", "player", ContainerType.AutosizedSingleImage);
+            Map.ContainerSetMaxSide("player", 100);
+            Map.ContainerSetCoordinate("player", 960, 540);
+            Platform.Map = Map;
+            Platform platform = new Platform();
+            platform.SetCoordinate(960, 600);
+            Platforms.Add(platform);
+
+            timer.AddAction(gameCycle, 10);
+        }
+        void addPictures()
+        {
+            Map.Library.AddPicture("A", "A.png");
+            Map.Library.AddPicture("player", "player.png");
+        }
+
+        void gameCycle()
+        {
+            for (int i = 0; i < Platforms.Count; i++)
+            {
+                Platforms[i].SetCoordinate(0, (int)speed);
+            }
+
+            if(speed > -1)
+            {
+                speed = speed - 0.05;
+            }
+        }
+
+        void checkKey(Key k)
+            {
+                if (k == Key.Space)
+                {
+                    speed = 5;
+                }
+
+            if (k == Key.LeftAlt)
+            {
+                for(int i = 0; i < Platforms.Count; i++)
+                {
+                    Platforms[i].SetCoordinate(20, 0);
+                }
+            }
+            if (k == Key.RightAlt)
+            {
+                speed = 5;
+            }
+        }
+
+        
+    }
+    class Platform
+    {
+        public int x;
+        public int y;
+        public string name;
+        static public UniversalMap_Wpf Map;
+        static int counter = 0;
+        public Platform()
+        {
+            //создать контейнер и его размер и нерисооооооовать егооооооооооооооооооооо0p
+            name = "Platform" + counter;
+            counter++;
+            Map.Library.AddContainer(name, "A", ContainerType.AutosizedSingleImage);
+            Map.ContainerSetMaxSide(name, 500);
+            
+        }
+
+        public void SetCoordinate(int dx, int dy)
+        {
+            x = x + dx;
+            y = y + dy;
+            Map.ContainerSetCoordinate(name, x, y);
+        }
+    }
+}
